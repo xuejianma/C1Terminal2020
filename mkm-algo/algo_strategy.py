0bf45e9d726f6cf3a00cdcafe500b625d400dbe2
Xuejian Ma <Xuejian.Ma@gmail.com>
2020-10-11 18:53:25 -0500

    update

--- mkm-algo/algo_strategy.py
+++ mkm-algo/algo_strategy.py
@@ -5,7 +5,6 @@ import warnings
 from sys import maxsize
 import json
 
-
 """
 Most of the algo code you write will be in this file unless you create new
 modules yourself. Start by modifying the 'on_turn' function.
@@ -19,36 +18,66 @@ Advanced strategy tips:
   the actual current map state.
 """
 
+
 class AlgoStrategy(gamelib.AlgoCore):
     def __init__(self):
         super().__init__()
         seed = random.randrange(maxsize)
         random.seed(seed)
         gamelib.debug_write('Random seed: {}'.format(seed))
-        self.firstFactories = [[13,2]] # upgrade
-        self.firstTurrets = [[3,12],[24,12]] # upgrade
+        self.firstFactories = [[13, 2]]  # upgrade
+        self.firstTurrets = [[3, 12], [24, 12]]  # upgrade
         self.firstWalls = []
 
-        self.secondFactories = [14,2]
-        self.secondTurrets = [2,13]
-        self.secondWalls = [[0,13],[1,13],[12,3],[11,4]]
+        self.secondFactories = [[14, 2]]
+        self.secondTurrets = [[2, 13]]
+        self.secondWalls = [[0, 13], [1, 13], [12, 3], [11, 4]]
 
-        self.thirdFactories = [[13,3]]
+        self.thirdFactories = [[13, 3]]
         self.thirdTurrets = []
-        self.thirdWalls = [[26,13],[27,13],[11,4],[10,5],[]]
+        self.thirdWalls = [[26, 13], [27, 13], [10, 5], [9, 6], [8, 7], [7, 8], [6, 9], [5, 10], [4, 11], [15, 3],
+                           [16, 4], [17, 5], [18, 6], [19, 7]]
 
+        self.fourthFactories = []
+        self.fourthTurrets = [[24, 11], [24, 10], [22, 11], [22, 10]]
+        self.fourthWalls = [[18, 6], [19, 7], [20, 8], [21, 9], [25, 12], [22, 12]]
 
+        self.extraFactories = [[14,3],\
+                               [13,4],[14,4],[12,4],[15,4],\
+                               [13,5],[14,5],[12,5],[15,5],[11,5],[16,5],\
+                               [13,6],[14,6],[12,6],[15,6],[11,6],[16,6],[10,6],[17,6],\
+                               [13,7],[14,7],[12,7],[15,7],[11,7],[16,7],[10,7],[17,7],[9,7],[18,7],\
+                               [13,7],[14,7],[12,7],[15,7],[11,7],[16,7],[10,7],[17,7],[9,7],[18,7],[8,7],[19,7]]
+        self.extraTurrets = [[21,12],[21,11],[21,10],[26,12],[25,11],[25,13],[24,13]]
+        self.extraWalls = []
 
-        self.firstScouts = [[8,5]]
-        self.firstInterceptors = [[8,5],[19,5]] #x1 for round 1,2,3
+        self.extraConditionalTurrets = [[item,12] for item in range(4,21)]+[[item, 1] for item in range(5, 21)]
+        self.extraConditionalFactories = [[item, 8] for item in range(8, 20)] + [[item, 9] for item in range(7, 21)]
+
+        self.firstScouts = [[8, 5]]
+        self.firstInterceptors = [[8, 5], [19, 5]]  # x1 for round 1,2,3
         self.firstDemolishers = []
 
-        # self.
+        self.batchFrontScouts = [[7,6]]
 
+
+        self.allFactories = self.firstFactories+self.secondFactories+self.thirdFactories+self.fourthFactories+self.extraFactories
+        self.allTurrets = self.firstTurrets+self.secondTurrets+self.thirdTurrets+self.fourthTurrets+self.extraTurrets
+        self.allWalls = self.firstWalls+self.secondWalls+self.thirdWalls+self.fourthWalls+self.extraWalls
+
+        self.vitalTurrets = self.firstTurrets+self.secondTurrets+self.thirdTurrets+self.fourthTurrets
+        self.vitalWalls = [[0,13],[1,13],[26,13],[27,13],[22,12],[15,12]]
+        self.pathCorners = [[0, 13],[1,13]]
+        self.pathBlock = [[9,4]]
+        self.batchAttackStep = 0
+
+        self.allFactoriesComplement = [item for item in self.allFactories if item not in self.pathCorners]
+        self.allTurretsComplement = [item for item in self.allTurrets if item not in self.pathCorners]
+        self.allWallsComplement = [item for item in self.allWalls if item not in self.pathCorners]
 
     def on_game_start(self, config):
-        """ 
-        Read in config and perform any initial setup here 
+        """
+        Read in config and perform any initial setup here
         """
         gamelib.debug_write('Configuring your custom algo strategy...')
         self.config = config
@@ -74,24 +103,22 @@ class AlgoStrategy(gamelib.AlgoCore):
         """
         game_state = gamelib.GameState(self.config, turn_state)
         gamelib.debug_write('Performing turn {} of your custom algo strategy'.format(game_state.turn_number))
-        game_state.suppress_warnings(True)  #Comment or remove this line to enable warnings.
+        game_state.suppress_warnings(True)  # Comment or remove this line to enable warnings.
 
         # self.starter_strategy(game_state)
         self.mkm_strategy(game_state)
 
         game_state.submit_turn()
 
-
     """
     NOTE: All the methods after this point are part of the sample starter-algo
     strategy and can safely be replaced for your custom algo.
     """
-    def mkm_strategy(self,game_state):
+
+    def mkm_strategy(self, game_state):
         self.build_defences(game_state)
-        self.build_reactive_defense(game_state)
+        # self.build_reactive_defense(game_state)
         self.attack(game_state)
-
-
 
     def starter_strategy(self, game_state):
         """
@@ -147,62 +174,124 @@ class AlgoStrategy(gamelib.AlgoCore):
         # # upgrade walls so they soak more damage
         # game_state.attempt_upgrade(wall_locations)
         if game_state.turn_number == 0:
-            game_state.attempt_spawn(FACTORY,self.firstFacories)
-            game_state.attempt_upgrade(self.firstFacories)
+            game_state.attempt_spawn(FACTORY, self.firstFactories)
+            game_state.attempt_upgrade(self.firstFactories)
             game_state.attempt_spawn(TURRET, self.firstTurrets)
             game_state.attempt_upgrade(self.firstTurrets)
         elif game_state.turn_number == 1:
             pass
         elif game_state.turn_number == 2:
-            game_state.attempt_spawn(FACTORY, self.secondFacories)
+            game_state.attempt_spawn(FACTORY, self.secondFactories)
             game_state.attempt_spawn(TURRET, self.secondTurrets)
-            game_state.attempt_spawn(WALL,self.secondWalls)
+            game_state.attempt_spawn(WALL, self.secondWalls)
         elif game_state.turn_number == 3:
             game_state.attempt_upgrade(self.secondFactories)
         elif game_state.turn_number == 4:
-            game_state.attempt_spawn(self.thirdFactories)
-            game_state.attempt_upgrade(self.secondTurrets)
+            game_state.attempt_spawn(FACTORY, self.thirdFactories)
+            game_state.attempt_upgrade(self.thirdFactories)
+            # game_state.attempt_upgrade(self.secondTurrets)
+        elif game_state.turn_number == 5:
+            game_state.attempt_spawn(WALL, self.thirdWalls)
+        elif game_state.turn_number == 6:
+            game_state.attempt_spawn(TURRET, self.fourthTurrets)
+            game_state.attempt_spawn(WALL, self.fourthWalls)
+        else:
+            # print(self.batchAttackStep)
+            if self.batchAttackStep != 1:
+                if game_state.get_resource(SP)<=20 or game_state.turn_number<=20:
+                    game_state.attempt_spawn(TURRET, self.vitalTurrets)
+                else:
+                    game_state.attempt_spawn(TURRET,self.allTurrets)
+
+                if game_state.get_resource(SP)>=20:
+                    game_state.attempt_spawn(TURRET, self.allWalls)
+                else:
+                    game_state.attempt_spawn(WALL, self.allWalls)
+            else:
+                game_state.attempt_spawn(TURRET,self.allTurretsComplement)
+                if game_state.get_resource(SP)>=20:
+                    game_state.attempt_spawn(TURRET, self.allWallsComplement)
+                else:
+                    game_state.attempt_spawn(WALL, self.allWallsComplement)
+
+            game_state.attempt_upgrade(self.allTurrets)
+            game_state.attempt_upgrade(self.vitalWalls)
+            if game_state.turn_number>30:
+                game_state.attempt_upgrade(self.allWalls)
+
+            for i in range(len(self.allFactories)):
+                game_state.attempt_spawn(FACTORY, self.allFactories[i])
+                if i<=8 or game_state.get_resource(SP)>=50:
+                    game_state.attempt_upgrade(self.allFactories[i])
+
+            if game_state.get_resource(SP)>=30:
+                game_state.attempt_spawn(TURRET,self.extraConditionalTurrets)
+                game_state.attempt_upgrade(self.extraConditionalTurrets)
+                for i in range(len(self.extraConditionalFactories)):
+                    game_state.attempt_spawn(FACTORY, self.extraConditionalFactories[i])
+                    game_state.attempt_upgrade(self.extraConditionalFactories[i])
 
 
 
-    def attack(self,game_state):
-        if game_state.turn_number <=4:
-            game_state.attempt_spawn(INTERCEPTOR,self.firstInterceptors)
-            if game_state.turn_number == 3:
-                game_state.attempt_spawn(SCOUT, self.firstScouts,100)
+
+    def attack(self, game_state):
+        if game_state.turn_number <= 6:
+            game_state.attempt_spawn(INTERCEPTOR, self.firstInterceptors)
+            if game_state.turn_number in [3, 5]:
+                game_state.attempt_spawn(SCOUT, self.firstScouts, 1000)
+        else:
+            if game_state.turn_number<=30:
+                threshold = 70
+            else:
+                threshold = 150
+
+            if self.batchAttackStep == 2:
+                game_state.attempt_spawn(TURRET, self.pathCorners)
+                game_state.attempt_upgrade(self.pathCorners)
+                game_state.attempt_remove(self.pathBlock)
+                self.batchAttackStep = 0
+
+
+            elif self.batchAttackStep == 1:
+                game_state.attempt_spawn(WALL, self.pathBlock)
+                # game_state.attempt_spawn(SCOUT, self.batchFrontScouts, 30)
+                game_state.attempt_spawn(SCOUT, self.firstScouts, 1000)
+                self.batchAttackStep = 2
+
+            elif game_state.get_resource(MP)>threshold and self.batchAttackStep == 0:
+                game_state.attempt_remove(self.pathCorners)
+                self.batchAttackStep = 1
 
 
     def build_reactive_defense(self, game_state):
         """
         This function builds reactive defenses based on where the enemy scored on us from.
-        We can track where the opponent scored by looking at events in action frames 
+        We can track where the opponent scored by looking at events in action frames
         as shown in the on_action_frame function
         """
         for location in self.scored_on_locations:
             # Build turret one space above so that it doesn't block our own edge spawn locations
-            build_location = [location[0], location[1]+1]
+            build_location = [location[0], location[1] + 1]
             game_state.attempt_spawn(TURRET, build_location)
-
-    def attack(self,game_state):
-        pass
 
     def stall_with_interceptors(self, game_state):
         """
         Send out interceptors at random locations to defend our base from enemy moving units.
         """
         # We can spawn moving units on our edges so a list of all our edge locations
-        friendly_edges = game_state.game_map.get_edge_locations(game_state.game_map.BOTTOM_LEFT) + game_state.game_map.get_edge_locations(game_state.game_map.BOTTOM_RIGHT)
-        
-        # Remove locations that are blocked by our own structures 
+        friendly_edges = game_state.game_map.get_edge_locations(
+            game_state.game_map.BOTTOM_LEFT) + game_state.game_map.get_edge_locations(game_state.game_map.BOTTOM_RIGHT)
+
+        # Remove locations that are blocked by our own structures
         # since we can't deploy units there.
         deploy_locations = self.filter_blocked_locations(friendly_edges, game_state)
-        
+
         # While we have remaining MP to spend lets send out interceptors randomly.
         while game_state.get_resource(MP) >= game_state.type_cost(INTERCEPTOR)[MP] and len(deploy_locations) > 0:
             # Choose a random deploy location.
             deploy_index = random.randint(0, len(deploy_locations) - 1)
             deploy_location = deploy_locations[deploy_index]
-            
+
             game_state.attempt_spawn(INTERCEPTOR, deploy_location)
             """
             We don't have to remove the location since multiple mobile 
@@ -234,7 +323,7 @@ class AlgoStrategy(gamelib.AlgoCore):
     def least_damage_spawn_location(self, game_state, location_options):
         """
         This function will help us guess which location is the safest to spawn moving units from.
-        It gets the path the unit will take then checks locations on that path to 
+        It gets the path the unit will take then checks locations on that path to
         estimate the path's damage risk.
         """
         damages = []
@@ -244,21 +333,23 @@ class AlgoStrategy(gamelib.AlgoCore):
             damage = 0
             for path_location in path:
                 # Get number of enemy turrets that can attack each location and multiply by turret damage
-                damage += len(game_state.get_attackers(path_location, 0)) * gamelib.GameUnit(TURRET, game_state.config).damage_i
+                damage += len(game_state.get_attackers(path_location, 0)) * gamelib.GameUnit(TURRET,
+                                                                                             game_state.config).damage_i
             damages.append(damage)
-        
+
         # Now just return the location that takes the least damage
         return location_options[damages.index(min(damages))]
 
-    def detect_enemy_unit(self, game_state, unit_type=None, valid_x = None, valid_y = None):
+    def detect_enemy_unit(self, game_state, unit_type=None, valid_x=None, valid_y=None):
         total_units = 0
         for location in game_state.game_map:
             if game_state.contains_stationary_unit(location):
                 for unit in game_state.game_map[location]:
-                    if unit.player_index == 1 and (unit_type is None or unit.unit_type == unit_type) and (valid_x is None or location[0] in valid_x) and (valid_y is None or location[1] in valid_y):
+                    if unit.player_index == 1 and (unit_type is None or unit.unit_type == unit_type) and (
+                            valid_x is None or location[0] in valid_x) and (valid_y is None or location[1] in valid_y):
                         total_units += 1
         return total_units
-        
+
     def filter_blocked_locations(self, locations, game_state):
         filtered = []
         for location in locations:
